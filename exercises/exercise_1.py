3d724d42e904b6d6b7d47eb221e1303c6de3e9e2
DivineDDO <168651857+DivineDDO@users.noreply.github.com>
2024-05-10 08:50:51 +0000

    Upload solution

--- exercises/exercise_1.py
+++ exercises/exercise_1.py
@@ -1,2 +1,8 @@
 # Exercise 1
 # Your solution comes here
+five_digits= str(input("Enter a five digit number but it would be a string store: "))
+interger=int(five_digits)
+first_side= ((interger//10000)%10) + ((interger//100)%10) + ((interger//1)%10)
+second_side= ((interger//1000)%10) + ((interger//10)%10)
+new_number= (str(first_side))+(str(second_side))
+print(new_number)
--- exercises/exercise_4.py
+++ exercises/exercise_4.py
@@ -1,2 +1,12 @@
 # Exercise 4
 # Your solution comes here
+symmetry= int(input("Enter a four digit number: "))
+if symmetry>9999 or symmetry<1000:
+  print("The number entered is not four digits")
+else:
+  print("Entered number is four digits now checking for symmetry")
+str(symmetry)
+if symmetry[0]==symmetry[3] and symmetry[1]==symmetry[2]:
+  print(1)
+else:
+  print(0)
