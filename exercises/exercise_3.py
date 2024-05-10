5500c83b77060601318ff08cf5ccbfa5cfaa0834
DivineDDO <168651857+DivineDDO@users.noreply.github.com>
2024-05-10 08:52:08 +0000

    udate solution

--- exercises/exercise_3.py
+++ exercises/exercise_3.py
@@ -4,5 +4,5 @@ to_be= int(input("Enter a random set of digits to be turned into time: "))
 hours = (to_be // 3600) % 24
 minutes = (to_be // 60) % 60
 seconds = to_be % 60
-time=(f"{hours} : {minutes:02d} : {seconds:02d}")
+time=(f"{hours}:{minutes:02d}:{seconds:02d}")
 print(time)
