b952d0601f1f1e875d992fa671ae1e99fda277d3
DivineDDO <168651857+DivineDDO@users.noreply.github.com>
2024-05-13 10:34:07 +0000

    new new

--- exercises/exercise_6.py
+++ exercises/exercise_6.py
@@ -2,5 +2,5 @@
 # Your solution comes here
 a= int(input("Enter a number: "))
 b= int(input("Enter a number: "))
-result = "Yes" if a%b==0 else "No"
+result = "YES" if a%b==0 else "NO"
 print(result)
