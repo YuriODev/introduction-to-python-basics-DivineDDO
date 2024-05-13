e600e1c020e949db4c318737b5f985f0ecbe8409
DivineDDO <168651857+DivineDDO@users.noreply.github.com>
2024-05-13 11:21:16 +0000

    is it?

--- exercises/exercise_7.py
+++ exercises/exercise_7.py
@@ -1,2 +1,9 @@
 # Exercise 7
 # Your solution comes here
+number=int(input("Enter a four digit number:"))
+thousands_digit = number // 1000
+hundreds_digit = (number % 1000) // 100
+tens_digit = (number % 100) // 10
+units_digit = number % 10
+sum_of_digits = thousands_digit + hundreds_digit + tens_digit + units_digit
+print(sum_of_digits)
